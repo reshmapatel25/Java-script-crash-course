cfbb15c0b44e2d2ae5718be8c93ebf4a4206ce16
Reshma Patel <reshma2505@gmail.com>
2019-10-30 20:28:15 +0000

    Add files via upload

---
+++ server.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const fs = require("fs-extra");
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+app.get("/", (request, response) => {
+  response.sendFile(__dirname + "/index.html");
+
+  /* this is way faster than doing
+  const fs = require("fs");
+  fs.readFile("./index.html", "utf-8", (err, data) => response.send(data));
+  */
+});
+
+app.get("/newsletter", (request, response) => {
+  response.sendFile(__dirname + "/newsletter.html");
+});
+
+app.post("/register", (request, response) => {
+  const newEmail = request.body.email;
+
+  const registrationListFileName = "data/registrationList.txt";
+  fs.ensureFile(registrationListFileName)
+    .then(() => fs.appendFile(registrationListFileName, `${newEmail}\n`));
+
+  response.send("request received, thank you");
+});
+
+// static file serving, see: https://expressjs.com/en/starter/static-files.html
+app.use(express.static("public"));
+
+app.listen(3000, () => console.log("server listening on port 3000"));
